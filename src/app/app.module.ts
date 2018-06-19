ed9439d3d500bdf44b0cac3881c3cd0f586bfe00
Brian Armolla <brian.armolla@gmail.com>
2018-06-19 11:53:51 -0300

    Se crea el proyecto inicial y las páginas necesarias para la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { HomePage, TabsPage, GuardadosPage, MapaPage } from '../pages/index.paginas';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    TabsPage,
+    GuardadosPage,
+    MapaPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    TabsPage,
+    GuardadosPage,
+    MapaPage
   ],
   providers: [
     StatusBar,
